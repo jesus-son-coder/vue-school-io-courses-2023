3228775b378b31ae6c2015c8f28fd2c815f6a8ae
hseka <hseka@lescollectionneurs.com>
2023-05-05 00:14:05 +0200

    - Editable : modifier la valeur des champs et Objets - Emit events + data - Lodash "findIndex"

--- 01-vue-interface/src/App.vue
+++ 01-vue-interface/src/App.vue
@@ -2,13 +2,17 @@
   <div id="main-app" class="container">
     <div class="row">
       <h4>Title</h4>
-        <div class="row button-container">
-          <font-awesome-icon icon="plus" class="mr-2" /> Add Appointment
-          <br><br>
-        </div>
-        <div class="row">
-          <appointment-list :appointments="appointments" @remove="removeItem" />
-        </div>
+      <div class="row button-container">
+        <font-awesome-icon icon="plus" class="mr-2" /> Add Appointment
+        <br /><br />
+      </div>
+      <div class="row">
+        <appointment-list
+          :appointments="appointments"
+          @remove="removeItem"
+          @edit="editItem"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -17,35 +21,43 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import _ from "lodash";
 import axios from "axios";
-import AppointmentList from '@/components/AppointmentList';
+import AppointmentList from "@/components/AppointmentList";
 
 export default {
-  name: 'MainApp',
+  name: "MainApp",
   components: {
     FontAwesomeIcon,
-    AppointmentList
+    AppointmentList,
   },
-  data: function() {
+  data: function () {
     return {
       title: "Appointment List",
       appointments: [],
-      aptIndex: 0
-    }
+      aptIndex: 0,
+    };
   },
   methods: {
-    removeItem: function(apt) {
+    removeItem: function (apt) {
       this.appointments = _.without(this.appointments, apt);
-    }
+    },
+    editItem: function (id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id,
+      });
+      this.appointments[aptIndex][field] = text;
+    },
   },
   mounted() {
-    axios.get("./data/appointments.json")
-      .then(response => (this.appointments = response.data.map(item => {
-        item.aptId = this.aptIndex;
-        this.aptIndex++;
-        return item;
-    })));
-  }
-}
+    axios.get("./data/appointments.json").then(
+      (response) =>
+        (this.appointments = response.data.map((item) => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+};
 </script>
 
 <style>
--- 01-vue-interface/src/components/AppointmentList.vue
+++ 01-vue-interface/src/components/AppointmentList.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="col-12 col-md-10 col-lg-7">
     <div class="list-group list-group-flush">
-      <div class="list-group-item d-flex align-items-start" v-for="(item, i) in appointments" v-bind:key="i">
-        <button class="mr-2 btn btn-sm btn-danger"
-                @click="$emit('remove', item)">
-          <font-awesome-icon icon="trash" class="mr-2"/>
+      <div
+        v-for="item in appointments"
+        :key="item.aptIndex"
+        class="list-group-item d-flex align-items-start"
+      >
+        <button
+          class="mr-2 btn btn-sm btn-danger"
+          @click="$emit('remove', item)"
+        >
+          <font-awesome-icon class="mr-2" icon="trash" />
         </button>
         <div class="w-100 apt-group">
           <div class="d-flex justify-content-between">
-            <span class="h4 text-primary">{{ item.petName }}</span>
+            <span
+              class="h4 text-primary"
+              contenteditable="contenteditable"
+              @blur="
+                $emit('edit', item.aptId, 'petName', $event.target.innerText)
+              "
+            >
+              {{ item.petName }}</span
+            >
             <span class="float-right">{{ formattedDate(item.aptDate) }}</span>
           </div>
           <div class="owner-name">
-            <span class="font-weight-bold text-primary mr-1">Owner:</span>
-            <span>{{ item.petOwner }}</span>
+            <span class="font-weight-bold text-primary mr-1">Owner: </span>
+            <span
+              contenteditable="contenteditable"
+              @blur="
+                $emit('edit', item.aptId, 'petOwner', $event.target.innerText)
+              "
+              >{{ item.petOwner }}</span
+            >
           </div>
-          <div>{{ item.aptNotes }}</div>
+          <div
+            contenteditable="contenteditable"
+            @blur="
+              $emit('edit', item.aptId, 'aptNotes', $event.target.innerText)
+            "
+          >
+            {{ item.aptNotes }}
+          </div>
         </div>
       </div>
     </div>
@@ -23,18 +50,18 @@
 </template>
 
 <script>
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import moment from 'moment';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import moment from "moment";
 
 export default {
-  name: 'AppointmentList',
+  name: "AppointmentList",
   components: {
     FontAwesomeIcon,
   },
-  props: ['appointments'],
+  props: ["appointments"],
   methods: {
-    formattedDate: function(date) {
-      return moment(new Date(date)).format('DD-MM-YYYY, HH:mm');
+    formattedDate: function (date) {
+      return moment(new Date(date)).format("DD-MM-YYYY, HH:mm");
     },
   },
 };
