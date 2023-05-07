5ad334b157c61a1c2e49cb21f5b3a1641bb837ed
jesus-son-coder <rvck2007@gmail.com>
2023-05-07 19:55:26 +0200

    dynamic routes

---
+++ pages/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Dynamic Routes</h1>
+    <p>This is the slug for this page : {{ $route.params.slug }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style></style>
