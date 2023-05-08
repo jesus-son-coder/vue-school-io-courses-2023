66473a8039f7e27da199ee8f0f2130547f6e8b4e
jesus-son-coder <rvck2007@gmail.com>
2023-05-08 22:33:01 +0200

    Variable complexe Reactive avec "reactive"

--- 03-vue3-composition-api/vue3-composition-api/src/App.vue
+++ 03-vue3-composition-api/vue3-composition-api/src/App.vue
@@ -2,31 +2,30 @@
   <h1>{{ name }}</h1>
   <input type="text" v-model="name"><br>
   <button @click="placeOrder">Place Order</button>
-  <yummy-meal name="Hamburger" :price="Number(5)" @addToCart="addItemToCart" />
+  <yummy-meal name="meal.name" :price="meal.price" @addToCart="addItemToCart" />
 </template>
 
 <script>
 import YummyMeal from "./components/YummyMeal.vue";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   components: { YummyMeal },
+
   setup() {
     const name = ref("The Snazzy Burger");
     name.value = "Hello from the setup function";
 
-    console.log("in setup", "--name.value--", name.value);
+    const meal = reactive({ name: "Hamburger", price: 5});
 
-    const placeOrder2 = () => alert("Yut order 2 has been placed !");
+    console.log(meal.name);
+
+    const placeOrder = () => alert("Yut order 2 has been placed !");
     const addItemToCart = (item) => alert(`Le produit ${item} a bien été ajouté au panier`);
 
-    return {name, placeOrder2, addItemToCart};
+    return {name, meal, placeOrder, addItemToCart};
   },
 
-  created() {
-    this.name = "Hello from the create hook";
-    console.log("in created", "--this.name--", this.name);
-  }
 }
 
 </script>
