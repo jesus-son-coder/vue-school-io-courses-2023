a656a7c7a044b66efcc3a3ecb7c3bde4c0299490
jesus-son-coder <rvck2007@gmail.com>
2023-05-08 12:07:20 +0200

    La méthode Setup de Composition APi

--- 03-vue3-composition-api/vue3-composition-api/src/App.vue
+++ 03-vue3-composition-api/vue3-composition-api/src/App.vue
@@ -1,30 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ name }}</h1>
+  <button @click="placeOrder2">Place Order</button>
+  <yummy-meal name="Hamburger" :price="Number(5)" @addToCart="addItemToCart" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import YummyMeal from "./components/YummyMeal.vue";
+
+export default {
+  components: { YummyMeal },
+  setup() {
+    const name = "The Snazzy Burger";
+
+    function placeOrder() {
+      alert('Your order has been placed');
+    }
+    // Utilisation de la syntaxe raccourcie :
+    const placeOrder2 = () => alert("Yut order 2 has been placed !");
+
+    const addItemToCart = (item) => alert(`Le produit ${item} a bien été ajouté au panier`);
+
+    return {name, placeOrder, placeOrder2, addItemToCart};
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ 03-vue3-composition-api/vue3-composition-api/src/components/YummyMeal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>
+      <strong>{{ name }}</strong>
+      {{ price }}€
+    </p>
+    <button @click="addToCart">Add to Cart</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number,
+  },
+  setup(props, { attrs, emit, slots}) {
+    const addToCart = () => emit('addToCart', props.name);
+    return { addToCart };
+  }
+}
+</script>
+
+<style>
+</style>
